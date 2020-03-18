6956b490c285745e1d0dd4489283cacca6a9d2db
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2020-03-18 16:42:43 +0700

    Update

---
+++ build/precache-manifest.7da43a736ee42547a913e55bbb023b7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0e8384b5b667c4fdd51d60eb3e1bbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b039deb626c2518a0494",
+    "url": "/static/css/2.0e36740a.chunk.css"
+  },
+  {
+    "revision": "e6ba4184643a116a6603",
+    "url": "/static/css/main.db66915a.chunk.css"
+  },
+  {
+    "revision": "b039deb626c2518a0494",
+    "url": "/static/js/2.0e9126c0.chunk.js"
+  },
+  {
+    "revision": "97f05533289e9bf9f24305d6e5358a9b",
+    "url": "/static/js/2.0e9126c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ba4184643a116a6603",
+    "url": "/static/js/main.4506af0f.chunk.js"
+  },
+  {
+    "revision": "c5ebeae10d7c553901cd",
+    "url": "/static/js/runtime-main.19232b0b.js"
+  },
+  {
+    "revision": "196958a5acf44fd25c54de8d685d327b",
+    "url": "/static/media/logo.196958a5.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b081e909b0abd61d6e3557ccf680f07.js"
+  "/precache-manifest.7da43a736ee42547a913e55bbb023b7c.js"
 );
 
 self.addEventListener('message', (event) => {
