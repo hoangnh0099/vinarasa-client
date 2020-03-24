5b08e1d6740f572bbdc4d95e338377f1a03d7a9a
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2020-03-25 00:05:53 +0700

    Add to cart Home

---
+++ build/precache-manifest.4aefc4a6ab20be46f2be184fbb9a0786.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3915d82b2eed0a51b6f9f5a135507ea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82a60c646605b5d4096c",
+    "url": "/static/css/2.0e36740a.chunk.css"
+  },
+  {
+    "revision": "126ac912d413bc2bccae",
+    "url": "/static/css/main.c0157a93.chunk.css"
+  },
+  {
+    "revision": "82a60c646605b5d4096c",
+    "url": "/static/js/2.2ab563a6.chunk.js"
+  },
+  {
+    "revision": "3507b1f75f0c6e1fa77c5ce12506cbd7",
+    "url": "/static/js/2.2ab563a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126ac912d413bc2bccae",
+    "url": "/static/js/main.6634e4bc.chunk.js"
+  },
+  {
+    "revision": "c5ebeae10d7c553901cd",
+    "url": "/static/js/runtime-main.19232b0b.js"
+  },
+  {
+    "revision": "196958a5acf44fd25c54de8d685d327b",
+    "url": "/static/media/logo.196958a5.png"
+  }
+]);
